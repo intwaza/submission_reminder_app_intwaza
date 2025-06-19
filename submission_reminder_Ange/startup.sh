5053a79696007477fa3eb82dee718f0a632ea102
Belyse <iintwazabelyse@gmail.com>
2025-06-19 12:27:03 +0200

    add readme

--- submission_reminder_Ange/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start reminder script if it exists
-if [ -f "./app/reminder.sh" ]; then
-    echo "Starting the reminder script..."
-    bash ./app/reminder.sh
-else
-    echo "Reminder script not found. Exiting."
-fi
